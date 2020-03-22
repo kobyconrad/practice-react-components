bc5b7b15889a88e268e720d9af4a59c46fd34c27
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-03-22 16:55:32 -0700

    created first component

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // TODO
+var GroceryList = () => (
+  <div>
+    <ul>
+      <li>Noodles</li>
+      <li>Chicken</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
