6081be57a92a4076c363012db344a9fb56594289
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-03-22 17:07:19 -0700

    added todo props component

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 // TODO
+var TodoList = props => {
+  return (
+    <ol>
+      <li>{props.todos[0]}</li>
+      <li>{props.todos[1]}</li>
+      <li>{props.todos[2]}</li>
+    </ol>
+  );
+};
+
 var GroceryList = () => (
   <div>
-    <ul>
-      <Noodles />
-      <Chicken />
-    </ul>
+    <TodoList todos={["Noodles", "Chicken", "Salt"]} />
   </div>
 );
 
-var Noodles = () => <li>Noodles</li>;
+// var Noodles = () => <li>Noodles</li>;
 
-var Chicken = () => <li>Chicken</li>;
+// var Chicken = () => <li>Chicken</li>;
 
 ReactDOM.render(<GroceryList />, document.getElementById("app"));
